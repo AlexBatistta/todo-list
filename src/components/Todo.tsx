c236e875ba71d1f69e815e769c9b938b9a6c2c59
Alex Batistta <batistta.alex@outlook.com>
2025-05-07 19:36:17 -0300

    input zones bigger

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -36,50 +36,53 @@ export const Todo: React.FC<Props> = ({
 }) => {
 	return (
 		<div
-			className={`flex justify-between items-center px-3 py-2 rounded-lg shadow-md gap-4 ${priorityClasses[priority]} w-full overflow-hidden`}
+			className={`flex justify-between items-center py-2 rounded-lg shadow-md gap-4 ${priorityClasses[priority]} w-full overflow-hidden`}
 		>
 			<div
-				className="text-2xl text-slate-500 cursor-grab active:cursor-grabbing"
+				className='relative text-2xl flex justify-center items-center  text-slate-500 cursor-grab active:cursor-grabbing w-6 h-7'
 				{...dragHandleProps?.attributes}
 				{...dragHandleProps?.listeners}
 			>
-				<RiDraggable />
+				<RiDraggable className='absolute' />
 			</div>
 
-			<div className="relative w-6 h-6 flex items-center justify-center">
+			<div className='relative flex items-center justify-center'>
 				<input
-					className="appearance-none w-4 h-4 cursor-pointer border-2 rounded-full border-slate-500 checked:border-slate-700/10"
-					type="checkbox"
+					className='appearance-none w-8 h-7 cursor-pointer'
+					type='checkbox'
 					id={id}
 					checked={completed}
 					onChange={() => {
 						console.log('Toggle Todo', id);
 					}}
 				/>
+				<div
+					className={`absolute pointer-events-none w-4 h-4 border-2 rounded-full  ${completed ? 'border-slate-500/25' : 'border-slate-500'}`}
+				/>
 				{completed && (
 					<FaCheck
-						className="absolute text-green-500 pointer-events-none"
+						className='absolute text-green-500 pointer-events-none'
 						size={20}
 					/>
 				)}
 			</div>
 
-			<label className="font-medium text-xl text-slate-700 flex-grow truncate overflow-hidden whitespace-nowrap w-full">
+			<label className='font-medium text-xl text-slate-700 flex-grow truncate overflow-hidden whitespace-nowrap w-full'>
 				{title}
 			</label>
 
-			<div className="relative w-5 h-5">
+			<div className='relative flex items-center justify-center'>
 				<FaRegTimesCircle
-					className="absolute text-slate-500 pointer-events-none flex-grow truncate overflow-hidden whitespace-nowrap"
+					className='absolute text-slate-500 pointer-events-none flex-grow truncate overflow-hidden whitespace-nowrap'
 					size={20}
 				/>
 				<button
-					className="absolute w-5 h-5 cursor-pointer"
+					className='absolute w-8 h-7 cursor-pointer'
 					onClick={() => onRemove(id)}
 				/>
 			</div>
-			<label className="hidden">{description}</label>
-			<div className="relative flex items-center justify-start">
+			<label className='hidden'>{description}</label>
+			<div className='relative flex items-center justify-end w-6 h-7'>
 				<div
 					className={`absolute h-14 w-3 ${priorityBarColors[priority]}`}
 				/>
