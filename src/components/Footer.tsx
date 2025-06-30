5573c55f4b7e64f4c87f6606b6313402243a24c5
Alex Batistta <batistta.alex@outlook.com>
2025-06-30 19:52:44 -0300

    fix: update footer to include a link to the developer's GitHub profile

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,13 @@ export const Footer: React.FC = () => {
 	return (
 		<footer>
 			<p className='mt-2 text-center text-sm text-slate-500'>
-				Developed by Alex Batistta
+				Developed by{' '}
+				<a
+					className='cursor-pointer font-bold transition-colors duration-200 hover:underline'
+					href='https://github.com/AlexBatistta'
+				>
+					Alex Batistta
+				</a>
 			</p>
 		</footer>
 	);
